7f7dca03fc6929ff60ee4761fda5a4c89d45e8ce
chairuizhong <chairuizhong@bigo.sg>
2020-10-22 16:50:03 +0800

    part 2 common students and part 3 suspended complete

--- server.py
+++ server.py
@@ -6,7 +6,9 @@ import json
 mgc = pymongo.MongoClient(host='localhost', port=27017)
 db = mgc.test
 teacher_student_table = db.teacher_student_table
+suspend_student_table = db.suspend_student_table
 #teacher_student_table.drop()
+#suspend_student_table.drop()
 
 app = Flask(__name__)
 
@@ -14,27 +16,49 @@ app = Flask(__name__)
 def register():
 	register_form = request.json
 	result = teacher_student_table.find_one({'teacher':register_form['teacher']})
-	print(result)
 	if not result:
 		record = {'teacher': register_form['teacher'], 'students': register_form['students']}
 		teacher_student_table.insert_one(record)
 	else:
-		print(result['_id'])
 		current_student_info = result['students']
+
 		#update in mongo replaces instead of append, therefore manual append and remove duplicates
 		unique_student_info = set(current_student_info + register_form['students'])
-		print(unique_student_info)
 		teacher_student_table.update_one({'students': current_student_info}, 
 										 {"$set": {'students':list(unique_student_info)}})
-	return 'success', 204
+	return json.dumps({'message':'success'}), 204
 
 @app.route('/api/commonstudents', methods=['GET'])
 def retrieve():
-	pass
+	teacher_list = request.args.getlist('teacher')
+	common_students = []
+
+	#given a list of teachers, find intersection using set to get common students
+	for teacher in teacher_list:
+		result = teacher_student_table.find_one({'teacher': teacher})
+		if not result:
+			return json.dumps({'message':'No such teacher:{} is registered!'.format(teacher)}), 404
+		if common_students == []:
+			common_students = result['students']
+		else:
+			common_students = set(common_students) & set(result['students'])
+
+	if len(list(common_students)) == 0:
+		return json.dumps({'message':'No common students found!'.format(teacher)}), 200
+
+	return json.dumps({'students':list(common_students)}), 200
 
 @app.route('/api/suspend', methods=['POST'])
 def suspend():
-	pass
+	suspend_form = request.json
+
+	#check if the student has already been suspended to avoid duplicate entries
+	result = suspend_student_table.find_one({'student':suspend_form['student']})
+	if result:
+		return json.dumps({'message':'student:{} has already been suspended before'.format(suspend_form['student'])}), 204
+
+	suspend_student_table.insert_one({'student':suspend_form['student']})
+	return json.dumps({'message':'success'}), 204
 
 @app.route('/api/retrievefornotifications', methods=['POST'])
 def notifications():
